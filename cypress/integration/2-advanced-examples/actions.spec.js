861627c1c760ff9b5027e959c4e843817c8a8ae9
Jello Cho <jello.cho@linethree.com>
2021-10-28 16:23:16 +0900

    comment on actions.spec.js

--- cypress/integration/2-advanced-examples/actions.spec.js
+++ cypress/integration/2-advanced-examples/actions.spec.js
@@ -6,7 +6,6 @@ context('Actions', () => {
   })
 
   it('.type() - type into a DOM element', () => {
-
     cy.get('.action-email')
       .type('fake@email.com').should('have.value', 'fake@email.com')
 
